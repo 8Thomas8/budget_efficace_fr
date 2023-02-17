a03fd8e065ed7dcf69fb1da57482e96ee72f5bc5
Thomas Rousseau <thomas.rousseau@proton.me>
2023-02-17 01:26:15 +0100

    Update sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,17 +1,33 @@
 import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
 export default defineEventHandler(async event => {
-    // Fetch all documents
+    const ignoredPaths = [
+        '/blog',
+        '/blog/credit-et-dettes',
+        '/blog/depenses-et-budgeting',
+        '/blog/epargne-et-investissement',
+        '/blog/etudiants',
+        '/blog/factures',
+        '/blog/finances-personnelles',
+        '/blog/retraite',
+        '/legislation/cgu',
+        '/legislation/mentions-legales',
+        '/legislation/politique-de-confidentialite'
+    ]
+    // Create sitemap
+    // Fetch all documents but not '/blog/factures'
     const docs = await serverQueryContent(event).find()
     const sitemap = new SitemapStream({
         hostname: 'https://budget-efficace.fr/'
     })
-    for (const doc of docs) {
-        sitemap.write({
-            url: doc._path,
-            changefreq: 'daily'
-        })
-    }
+    docs.forEach((doc: any) => {
+        if (!ignoredPaths.includes(doc._path)) {
+            sitemap.write({
+                url: doc._path,
+                changefreq: 'daily'
+            })
+        }
+    })
     sitemap.end()
     return streamToPromise(sitemap)
 })
